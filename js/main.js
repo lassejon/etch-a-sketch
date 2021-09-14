e984266f45418b85d2f3410eab0fca211f06795c
Lasse Jon Marquardt Frederiksen <lasss@live.dk>
2021-09-14 08:57:33 +0200

    committing my change before I try to make it different

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
-numSquare = 32;
-numRowCol = Math.sqrt(numSquare)
+numRows = 128;
+numRowCol = Math.sqrt(numRows);
 
 board = document.createElement('div');
-board.classList.add('board')
+board.classList.add('board');
 
-for (let i = 0; i < numRowCol; i++) {
+for (let i = 0; i < numRows; i++) {
     row = document.createElement('div');
     row.classList.add('row')
-    for (let j = 0; j < numRowCol; j++) {
+    for (let j = 0; j < numRows; j++) {
         square = document.createElement('div');
-        square.classList.add('square')
-        row.appendChild(square)
+        square.classList.add('square');
+        row.appendChild(square);
     }
-    board.appendChild(row)
+    board.appendChild(row);
 }
 
 document.body.appendChild(board)
 
 squares = document.getElementsByClassName('square')
-
-for(const element of squares) {
-    
-    element.addEventListener('mouseenter', function (event) {
-        if (element.style.backgroundColor !== "black") {
-            element.style.backgroundColor = "black";
-        } else {
-            element.style.backgroundColor = "white";
-        }
+for(const square of squares) {
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = "black";
     })
 };
